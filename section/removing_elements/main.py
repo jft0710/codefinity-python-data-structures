505d9dce80606f76ea57454f1189f146cb584342
jft0710 <>
2026-08-19 19:28:52 +0000

    Removing Elements

---
+++ section/removing_elements/main.py
@@ -0,0 +1,9 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Deleting element
+if "Oslo" or "Sydney" in travel_wishlist:
+    travel_wishlist.remove("Oslo")
+    travel_wishlist.remove("Sydney")
+
+# Testing
+print('Updated list:', travel_wishlist)
