97b8568dcae103e74dcb44048660d63bc9dd409d
jft0710 <>
2026-08-21 02:25:24 +0000

    Dictionary Basics

---
+++ section/dictionary_basics/main.py
@@ -0,0 +1,7 @@
+# Write your code here
+library = {"Book 1":"The Great Gatsby", "By": "F. Scott Fitzgerald",
+           "Book 2":"1984", "Author": "George Orwell", 
+           "Book 3": "To Kill a Mockingbird", "Written by": "Harper Lee"}
+
+# Testing
+print('Library dictionary:', library)
